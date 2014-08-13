f0b1ba65b63fbd0b5da1b9300d80f56b72fe992e
Bernard Udu <udubernard@gmail.com>
2014-08-13 12:37:50 -0400

    Initialized objects as collection of <List>

--- SpringFramework/src/org/webdev/spring/Mammals.java
+++ SpringFramework/src/org/webdev/spring/Mammals.java
@@ -1,64 +1,33 @@
 package org.webdev.spring;
 
+import java.util.List;
+
 public class Mammals {
-	private Primate gibbons;
-	private Primate gorilla;
-	private Primate siamangs;
-	private Primate bonobos;
-	/**
-	 * @return the gibbons
-	 */
-	public Primate getGibbons() {
-		return gibbons;
-	}
-	/**
-	 * @param gibbons the gibbons to set
-	 */
-	public void setGibbons(Primate gibbons) {
-		this.gibbons = gibbons;
-	}
-	/**
-	 * @return the gorilla
-	 */
-	public Primate getGorilla() {
-		return gorilla;
-	}
-	/**
-	 * @param gorilla the gorilla to set
-	 */
-	public void setGorilla(Primate gorilla) {
-		this.gorilla = gorilla;
-	}
-	/**
-	 * @return the siamangs
-	 */
-	public Primate getSiamangs() {
-		return siamangs;
-	}
-	/**
-	 * @param siamangs the siamangs to set
-	 */
-	public void setSiamangs(Primate siamangs) {
-		this.siamangs = siamangs;
-	}
-	/**
-	 * @return the bonobos
-	 */
-	public Primate getBonobos() {
-		return bonobos;
-	}
-	/**
-	 * @param bonobos the bonobos to set
-	 */
-	public void setBonobos(Primate bonobos) {
-		this.bonobos = bonobos;
-	}
+	private List<Primate> primates;
 	
+	
+	
+	/**
+	 * @return the primates
+	 */
+	public List<Primate> getPrimates() {
+		return primates;
+	}
+
+
+	/**
+	 * @param primates the primates to set
+	 */
+	public void setPrimates(List<Primate> primates) {
+		this.primates = primates;
+	}
+
+
+
 	public void display() {
-		System.out.println(getGibbons().getApes() + " are lesser apes found in Asia");
-		System.out.println(getGorilla().getApes() + " are great apes found in Africa");
-		System.out.println(getSiamangs().getApes() + " are lesser apes found in Asia");
-		System.out.println(getBonobos().getApes() + " are lesser apes found in Africa");
+		for (Primate primate : primates) {
+			System.out.println(primate.getApes() + " are all examples of apes.");
+		}
 		
 	}
 	
