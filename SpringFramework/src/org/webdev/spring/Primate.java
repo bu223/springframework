7f5bc8ea6ae23f10d6cf42d3e76f7f3a780e3026
Bernard Udu <udubernard@gmail.com>
2014-08-13 10:25:22 -0400

    New class of primates.

---
+++ SpringFramework/src/org/webdev/spring/Primate.java
@@ -0,0 +1,23 @@
+package org.webdev.spring;
+
+public class Primate {
+	private String apes;
+
+	/**
+	 * @return the apes
+	 */
+	public String getApes() {
+		return apes;
+	}
+
+	/**
+	 * @param apes the apes to set
+	 */
+	public void setApes(String apes) {
+		this.apes = apes;
+	}
+	
+	
+
+	
+}
